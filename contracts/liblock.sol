43a26b78bc4aed324ee2556163758bdf06e313af
Nathan <nathan.pauchon.38@gmail.com>
2023-10-02 16:08:53 +0200

    feat: changed liblock contract

--- contracts/liblock.sol
+++ contracts/liblock.sol
@@ -3,24 +3,25 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Liblock is
     ERC20,
     ERC20Burnable,
-    AccessControl,
     ERC20Permit,
     ERC20Votes
 {
-    constructor() ERC20("Liblock", "LIB") ERC20Permit("Liblock") {
-        _mint(msg.sender, 1000 * 10**decimals());
-        _mint(address(this), 74999000 * 10**decimals());
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    }
+    AggregatorV3Interface internal dataFeed;
 
-    // The following functions are overrides required by Solidity.
+    constructor() ERC20("Liblock", "LIB") ERC20Permit("Liblock") {
+        _mint(address(this), 75000000 * 10**decimals());
+        setAdmin(msg.sender);
+        dataFeed = AggregatorV3Interface(
+            0x59F1ec1f10bD7eD9B938431086bC1D9e233ECf41
+        );
+    }
 
     function _afterTokenTransfer(
         address from,
@@ -42,5 +43,40 @@ contract Liblock is
         override(ERC20, ERC20Votes)
     {
         super._burn(account, amount);
+    }
+
+    address public admin;
+
+    modifier onlyAdmin(){
+        require(isAdmin(msg.sender));
+        _;
+    }
+
+    function setAdmin(address account) private
+    {
+        require(account != address(0), "Invalid address");
+        admin = account;
+    }
+
+    function isAdmin(address account) internal view returns(bool)
+    {
+        return admin == account;
+    }
+
+    function mint(uint256 amount)
+        external onlyAdmin
+    {
+        _mint(msg.sender, amount);
+    }
+
+    function getLastData() public view returns (uint80, int, uint, uint, uint80) {
+        (
+            uint80 roundID,
+            int answer,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = dataFeed.latestRoundData();
+        return (roundID, answer, startedAt, timeStamp, answeredInRound);
     }
 }
