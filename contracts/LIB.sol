baf043035c4dd43b22492b415af7caf31ddd9a08
Nathan <nathan.pauchon.38@gmail.com>
2023-10-06 20:43:29 +0200

    feat: add mapping for excluding address from fees

--- contracts/LIB.sol
+++ contracts/LIB.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract Liblock is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes {
-
     // Init section
 
     address internal admin;
@@ -25,9 +24,11 @@ contract Liblock is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes {
     uint16 public liblockFondationWalletShares = 200; //20%
     uint16 public zeroAddressShares = 75; //7.5%
 
+    mapping(address => bool) private excludedFromFee;
+
     constructor() ERC20("Liblock", "LIB") ERC20Permit("Liblock") {
-        _mint(address(this), 74500000 * 10**decimals());
-        _mint(address(msg.sender), 500000 * 10**decimals());
+        _mint(address(this), 74500000 * 10 ** decimals());
+        _mint(address(msg.sender), 500000 * 10 ** decimals());
         admin = address(msg.sender);
         liblockFondationWallet = admin;
     }
@@ -49,7 +50,7 @@ contract Liblock is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes {
         return admin == account;
     }
 
-    // overwrite required function 
+    // overwrite required function
 
     function _afterTokenTransfer(
         address from,
@@ -59,50 +60,48 @@ contract Liblock is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes {
         super._afterTokenTransfer(from, to, amount);
     }
 
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _mint(
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
         super._mint(to, amount);
     }
 
-    function _burn(address account, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _burn(
+        address account,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
         super._burn(account, amount);
     }
 
     // token exclusive function
 
     // Define the amout of fees a user will pay depending of the amount of token
-    function calculateFeePercentage(uint256 amount)
-        private
-        pure
-        returns (uint32)
-    {
-        if (amount >= 35000000 * 10**18) {
+    function calculateFeePercentage(
+        uint256 amount
+    ) private pure returns (uint32) {
+        if (amount >= 35000000 * 10 ** 18) {
             return 520000000; // 52% fee
-        } else if (amount >= 10000000 * 10**18) {
-            uint256 fee = ((amount - 10000000 * 10**18) / 10**18) * 8; // 0.0000008
+        } else if (amount >= 10000000 * 10 ** 18) {
+            uint256 fee = ((amount - 10000000 * 10 ** 18) / 10 ** 18) * 8; // 0.0000008
             return uint32(320000000 + fee); // 32% fee + 8 / 10**7 per full token
-        } else if (amount >= 1000000 * 10**18) {
-            uint256 fee = ((amount - 1000000 * 10**18) / 10**18) * 22; // 0.00000022
+        } else if (amount >= 1000000 * 10 ** 18) {
+            uint256 fee = ((amount - 1000000 * 10 ** 18) / 10 ** 18) * 22; // 0.00000022
             return uint32(120000000 + fee); // 12% fee + 22 / 10**7 per full token
-        } else if (amount >= 100000 * 10**18) {
-            uint256 fee = ((amount - 100000 * 10**18) / 10**18) * 83; // 0.0000083
+        } else if (amount >= 100000 * 10 ** 18) {
+            uint256 fee = ((amount - 100000 * 10 ** 18) / 10 ** 18) * 83; // 0.0000083
             return uint32(45000000 + fee); // 4.5% fee + 83 / 10**7 per full token
-        } else if (amount >= 10000 * 10**18) {
-            uint256 fee = ((amount - 10000 * 10**18) / 10**18) * 416; // 0.0000416
+        } else if (amount >= 10000 * 10 ** 18) {
+            uint256 fee = ((amount - 10000 * 10 ** 18) / 10 ** 18) * 416; // 0.0000416
             return uint32(7500000 + fee); // 0.75% fee + 416 / 10**7 per full token
-        } else if (amount >= 1000 * 10**18) {
-            uint256 fee = ((amount - 1000 * 10**18) / 10**18) * 500; // 0.00005
+        } else if (amount >= 1000 * 10 ** 18) {
+            uint256 fee = ((amount - 1000 * 10 ** 18) / 10 ** 18) * 500; // 0.00005
             return uint32(3000000 + fee); // 0.3% fee + 500 / 10**7 per full token
-        } else if (amount >= 100 * 10**18) {
-            uint256 fee = ((amount - 100 * 10**18) / 10**18) * 2222; // 0.0002222
+        } else if (amount >= 100 * 10 ** 18) {
+            uint256 fee = ((amount - 100 * 10 ** 18) / 10 ** 18) * 2222; // 0.0002222
             return uint32(1000000 + fee); // 0.1% fee + 2222 / 10**7 per full token
-        } else if (amount >= 10 * 10**18) {
-            uint256 fee = ((amount - 10 * 10**18) / 10**18) * 10000; // 0.001
+        } else if (amount >= 10 * 10 ** 18) {
+            uint256 fee = ((amount - 10 * 10 ** 18) / 10 ** 18) * 10000; // 0.001
             return uint32(100000 + fee); // 0.01% fee + 10000 / 10**7 per full token
         } else {
             return 100000; // 0.01% fee
@@ -142,26 +141,30 @@ contract Liblock is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes {
         uint256 amount
     ) internal virtual override {
         uint32 feePercentage = calculateFeePercentage(amount);
-        require(feePercentage < 65 * 10**7, "Max fee amount reached");
-        uint256 feeAmount = (amount * (feePercentage / 10**3)) / 10**6;
+        require(feePercentage < 65 * 10 ** 7, "Max fee amount reached");
+        uint256 feeAmount = (amount * (feePercentage / 10 ** 3)) / 10 ** 6;
         uint256 transferAmount = amount - feeAmount;
 
-        super._transfer(sender, recipient, transferAmount);
-        super._transfer(
-            sender,
-            devWallet,
-            (feeAmount * devWalletShares) / 10**3
-        );
-        super._transfer(
-            sender,
-            distributionContract,
-            (feeAmount * distributionContractShares) / 10**3
-        );
-        super._transfer(
-            sender,
-            liblockFondationWallet,
-            (feeAmount * liblockFondationWalletShares) / 10**3
-        );
-        super._burn(sender, (feeAmount * zeroAddressShares) / 10**3);
+        if (excludedFromFee[sender] || excludedFromFee[recipient]) {
+            super._transfer(sender, recipient, transferAmount);
+        } else {
+            super._transfer(sender, recipient, transferAmount);
+            super._transfer(
+                sender,
+                devWallet,
+                (feeAmount * devWalletShares) / 10 ** 3
+            );
+            super._transfer(
+                sender,
+                distributionContract,
+                (feeAmount * distributionContractShares) / 10 ** 3
+            );
+            super._transfer(
+                sender,
+                liblockFondationWallet,
+                (feeAmount * liblockFondationWalletShares) / 10 ** 3
+            );
+            super._burn(sender, (feeAmount * zeroAddressShares) / 10 ** 3);
+        }
     }
 }
