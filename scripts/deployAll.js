a954f14669759eaad24ce8e5bf988191e950a997
Nathan <nathan.pauchon.38@gmail.com>
2023-10-21 14:27:27 +0200

    feat: add txn after contracts deployment

--- scripts/deployAll.js
+++ scripts/deployAll.js
@@ -38,6 +38,30 @@ async function main() {
 
   const csvContent = `Liblock Address,${liblock.address}\nrLiblock Address,${rLIB.address}\nProposal Address,${proposal.address}\nDistributor Address,${distributor.address}\nStacking Address,${stacking.address}\n- * - * -\n`;
   fs.appendFileSync(csvFilePath, csvContent);
+
+  const liblockWithSigner = await Liblock.attach(liblock.address).connect(ethers.provider.getSigner());
+  const rliblockWithSigner = await rLiblock.attach(rLIB.address).connect(ethers.provider.getSigner());
+  const distributorWithSigner = await Distributor.attach(distributor.address).connect(ethers.provider.getSigner());
+
+  const transaction0 = await liblockWithSigner.setDistributionContract(stacking.address);
+  await transaction0.wait();
+
+  console.log("setDistributionContract set!");
+
+  const transaction1 = await liblockWithSigner.setAdmin(stacking.address);
+  await transaction1.wait();
+
+  console.log("admin set for Liblock!");
+
+  const transaction2 = await rliblockWithSigner.setAdmin(stacking.address);
+  await transaction2.wait();
+
+  console.log("admin set for rLiblock!");
+
+  const transaction3 = await distributorWithSigner.setAdmin(stacking.address);
+  await transaction3.wait();
+
+  console.log("admin set for Distributor!");
 }
 
 async function verifyContract(contractAddress, args=[]) {
